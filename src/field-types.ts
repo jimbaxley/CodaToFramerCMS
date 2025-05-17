d2f5f2932eb482dff68f2177d131cf41a9ff946f
Jim Baxley <jim.baxley@example.com>
2025-05-17 10:44:36 -0400

    Removed field-types

--- src/field-types.ts
+++
@@ -1,6 +0,0 @@
-import { type FieldDataEntryInput } from "framer-plugin"
-
-// Type guard to check if a field data entry is a date
-export function isDateField(entry: FieldDataEntryInput): entry is DateFieldDataEntryInput {
-    return entry.type === 'date';
-}
